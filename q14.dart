f5d9513e1e323351cbb58fb66e2c7338f9c3632b
mohandhesham4848 <mohandhesham4848@gmail.com>
2025-02-20 06:04:05 +0200

    Delete q14.dart

--- q14.dart
+++
@@ -1,7 +0,0 @@
-/*Write a Dart program that declares a String variable named greeting with an initial
-value of "Hello". Then, update greeting to "Hello, Dart!" and print it. */
-void main () {
-  String greeting = "hello";
-greeting = "hello,dart";
-print(greeting);
-}
