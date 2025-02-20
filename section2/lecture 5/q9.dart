cd7dbbc936c0a47a52bd9a388b2940cd38cb0417
mohandhesham4848 <mohandhesham4848@gmail.com>
2025-02-20 15:32:51 +0200

    Delete section2/lecture 5 directory

--- section2/lecture 5/q9.dart
+++
@@ -1,9 +0,0 @@
-/*9. OOP, Constructors & Getters
-Create a class `Rectangle` with:
-- Private attributes `_width` and `_height`.
-- A constructor that initializes the values.
-- A getter `area` that calculates and returns the area.
-In `main()`, create a `Rectangle` object and print its area */
-class Rectangle {
-  
-}
