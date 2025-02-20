1acde0a68c877aab4b6d017cc8e3a5deb222363e
mohandhesham4848 <mohandhesham4848@gmail.com>
2025-02-20 02:39:53 +0200

    Delete q11.dart

--- q11.dart
+++
@@ -1,8 +0,0 @@
-/*How do you concatenate strings in Dart? Write an example using two variables:
-firstName and lastName.*/
-void main () {
-  String firstName="firstName";
-    String lastName="lastName";
-    String result = firstName +" "+ lastName ;
-    print( result);
-}
