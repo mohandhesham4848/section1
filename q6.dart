8cd687b55b33e9a984ee4c8482682746fdbb4648
mohandhesham4848 <mohandhesham4848@gmail.com>
2025-02-20 06:05:18 +0200

    Delete q6.dart

--- q6.dart
+++
@@ -1,6 +0,0 @@
-/*write a Dart program that declares a variable with a double data type, assigns it a
-value of 12.5, and prints it to the console. */
-void main () {
-  double number =12.5 ;
-  print(number);
-}
