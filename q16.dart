34923484f0f8783b88559a942bc2cc14ea6c6733
mohandhesham4848 <mohandhesham4848@gmail.com>
2025-02-20 06:04:25 +0200

    Delete q16.dart

--- q16.dart
+++
@@ -1,9 +0,0 @@
-/*Write a Dart code snippet that demonstrates the use of an arithmetic operator to
-calculate the remainder of dividing 10 by 3. */
-
-void main (){
-  int dividend =10 ; 
-  int divisor = 3;
-int remainder = dividend%divisor ;
-print("The remainder of $dividend divided by $divisor is $remainder");
-}
