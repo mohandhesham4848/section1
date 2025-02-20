d44b89d5c6b538ca37792e7e43e333b675719e13
mohandhesham4848 <mohandhesham4848@gmail.com>
2025-02-20 06:05:02 +0200

    Delete q4.dart

--- q4.dart
+++
@@ -1,9 +0,0 @@
-/*How can you declare a variable in Dart to store a person's age and ensure it can
-only hold integer values? Write the code. */
-
-
-void main () {
-  int person_age=19;
-  print(person_age);
-
-}
