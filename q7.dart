c814d85edf1708866b90b2aa06ea124b3ecb18fd
mohandhesham4848 <mohandhesham4848@gmail.com>
2025-02-20 06:05:28 +0200

    Delete q7.dart

--- q7.dart
+++
@@ -1,9 +0,0 @@
-/*
-
-Given this code, identify any errors and explain why they occur: ```dart void main()
-{ String name = "Alice"; name = 123; print(name); } ```
-*/
-
-
-   void main()
-{ String name = "Alice"; name = '123'; print(name);}
