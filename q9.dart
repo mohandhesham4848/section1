cba33b1f28a7eb20adbfffd3a747cb6cc9c7562c
mohandhesham4848 <mohandhesham4848@gmail.com>
2025-02-20 06:05:44 +0200

    Delete q9.dart

--- q9.dart
+++
@@ -1,7 +0,0 @@
-/*write code that declares a bool variable called isLoggedIn and assigns it a value of
-false. Then, change the value to true and print the result */
-void main (){
-  bool isLoggedIn = false ;
-  isLoggedIn = true ;
-  print(isLoggedIn);
-}
