9545958a53818b0cec7cbf739a710865a77631f2
mohandhesham4848 <mohandhesham4848@gmail.com>
2025-02-20 06:04:17 +0200

    Delete q15.dart

--- q15.dart
+++
@@ -1,4 +0,0 @@
-/*What does it mean for Dart to be a statically-typed language, and how does it
-impact the way variables are used? */
-
-//it means if we dclara variable as string we can't assigned variableas int 
