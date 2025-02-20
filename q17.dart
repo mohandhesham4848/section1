7551de0e8681a8212fe3c8a3c005ee026810b772
mohandhesham4848 <mohandhesham4848@gmail.com>
2025-02-20 06:04:31 +0200

    Delete q17.dart

--- q17.dart
+++
@@ -1,8 +0,0 @@
-/*If you need a variable that can hold any type of value and may change during
-execution, which data type would you use? Write a code example to show this */
-//dynamic
-void main(){
-  dynamic data1 =1 ;
-  data1 ="mohand";
-  print(data1);
-}
