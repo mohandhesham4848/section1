f99e09e9efec793b4839abdd3fc97281268865b8
mohandhesham4848 <mohandhesham4848@gmail.com>
2025-02-20 06:03:58 +0200

    Delete q13.dart

--- q13.dart
+++
@@ -1,8 +0,0 @@
-/*hat will be the output of this code snippet? Explain your answer: ```dart void
-main() { int x = 5; int y = x + 2 * 3; print(y); }  */
-void main () {
-  int x = 5 ; // now we have x==5
-  int y =x+2*3 ; //y=5+2*3>>5+6>>11
-  print(y);
-  
-}
